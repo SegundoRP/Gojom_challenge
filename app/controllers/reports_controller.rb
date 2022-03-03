f906d37c0badfbb284de20823a6ac6db53a93759
segundo rebaza <alvaro.rp1593@gmail.com>
2022-03-03 17:58:16 -0500

    se completo logica del formulario y el id randomico de show

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,24 @@
+class ReportsController < ApplicationController
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to report_path(@report)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:asunto, :descripcion, :fecha_del_incidente, photos:[])
+  end
+end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,17 @@
 class Report < ApplicationRecord
+  before_create :generate_random_id
+
+  validates :asunto, presence: true
+  validates :descripcion, presence: true
+  has_many_attached :photos
+  validates :photos, presence: true
+  has_rich_text :descripcion
+
+  private
+
+  def generate_random_id
+    begin
+      self.id = SecureRandom.uuid
+    end while Report.where(id: self.id).exists?
+  end
 end
