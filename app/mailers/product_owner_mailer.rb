9ca7a2389bfdb0cbeb14d52bcb40f61018a6e3e6
segundo rebaza <alvaro.rp1593@gmail.com>
2022-03-04 11:10:59 -0500

    se agrega formato mjml falta displayar imagenes

--- app/mailers/product_owner_mailer.rb
+++ app/mailers/product_owner_mailer.rb
@@ -1,6 +1,9 @@
 class ProductOwnerMailer < ApplicationMailer
   def report_email
     @report = params[:report]
-    mail to: 'andres.r4793@gmail.com', subject: 'Nuevo reporte de cliente'
+    mail(to: 'andres.r4793@gmail.com', subject: 'Nuevo reporte de cliente') do |format|
+      format.text
+      format.mjml
+    end
   end
 end
