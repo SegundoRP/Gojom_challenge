bbbdd5489a19bc933e318f9ecd5803c6b5319d40
segundo rebaza <alvaro.rp1593@gmail.com>
2022-03-04 16:36:08 -0500

    se hizo template mjml

--- app/mailers/product_owner_mailer.rb
+++ app/mailers/product_owner_mailer.rb
@@ -2,7 +2,6 @@ class ProductOwnerMailer < ApplicationMailer
   def report_email
     @report = params[:report]
     mail(to: 'andres.r4793@gmail.com', subject: 'Nuevo reporte de cliente') do |format|
-      format.text
       format.mjml
     end
   end
