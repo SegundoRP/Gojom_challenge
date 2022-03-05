ff787108afb9c8405eec562cb7d1e1d53cb9f376
segundo rebaza <alvaro.rp1593@gmail.com>
2022-03-05 14:27:51 -0500

    correcion

--- app/mailers/product_owner_mailer.rb
+++ app/mailers/product_owner_mailer.rb
@@ -1,4 +1,5 @@
 class ProductOwnerMailer < ApplicationMailer
+  # Metodo para establecer los parametros del correo como destinatario entre otros
   def report_email
     @report = params[:report]
     mail(to: 'casos@gojom.com', subject: 'Nuevo reporte de cliente') do |format|
