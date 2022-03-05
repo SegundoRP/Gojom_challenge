f70532e027267ebfd1e0b671a065b41379f5518a
segundo rebaza <alvaro.rp1593@gmail.com>
2022-03-05 01:54:26 -0500

    se corrigio random url, listas vistas con slim

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # Metodo para direccionar al usuario a pagina new report en cuanto se loguea
+  def after_sign_in_path_for(_resource)
+    new_report_path
+  end
 end
